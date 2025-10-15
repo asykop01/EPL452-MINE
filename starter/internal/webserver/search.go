61fd8493b70a15c8940028d4dc186e689a2fd677
asykop01 <asykop01@node0.asykop01-274217.ucy-coast-teach-pg0.utah.cloudlab.us>
2025-10-15 01:06:38 -0600

    changes

--- starter/internal/webserver/search.go
+++ starter/internal/webserver/search.go
@@ -1,17 +1,36 @@
-
 package webserver
-
 import (
-	"net/http"
+        "net/http"
+        "os"
+        "strings"
+	"encoding/json"
 )
 
-// RootHandler serves HTML content loaded from a file
-func (s *WebServer) RootHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement me
+func (s *WebServer) RootHandler(w http.ResponseWriter, r *http.Request){
+        htmlContent, err := os.ReadFile(s.htmlPath)
+        if err != nil{
+                http.Error(w, "Unable to load HTML file", http.StatusInternalServerError)
+        return
+        }
+        w.Header().Set("Content-Type","text/html")
+        w.Write(htmlContent)
 }
 
-// SearchHandler handles GET requests to the `/api/search` endpoint.
-// It expects a query parameter `?q=...` containing space-separated keywords.
-func (s *WebServer) SearchHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement me
+func (s *WebServer) SearchHandler(w http.ResponseWriter, r *http.Request){
+        query := r.URL.Query().Get("q")
+        if query == ""{
+                http.Error(w, "Missing query parameter", http.StatusBadRequest)
+        return
+        }
+        keywords := strings.Fields(strings.ToLower(query))
+        results, err := s.index.SearchInMemoryTopK(keywords,s.topK)
+        if err != nil{
+                http.Error(w, "Internal Server Error", http.StatusBadRequest)
+        return
+        }
+
+        w.Header().Set("Content-Type","application/json")
+        json.NewEncoder(w).Encode(results)
 }
+
+
